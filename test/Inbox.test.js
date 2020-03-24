c28b42c0e7335ebb133d865e10a921bc6a7b68df
Alan Tran <dev@protoalpha.co>
2020-03-23 23:22:33 -0500

    Remove unneeded logs & test code

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,42 +2,24 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-// const { interface, bytecode } = require('../compile');
+const { interface, bytecode } = require('../compile');
 
-// let accounts;
-// let inbox;
-
-
-// beforeEach(async () => {
-//     // Get a list of all accounts
-//     accounts = await web3.eth.getAccounts();
-
-//     // User one of those accounts to deploy the contract
-//     // inbox = await new web3.eth.Contract(JSON.parse(interface))
-//     //     .deploy( { data: bytecode, arguments: ['Hi there!'] })
-//     //     .send({ from: accounts[0], gas: '1000000'});
-// });
-
-// describe('Inbox', () => {
-//     it('deploys a contract', () => {
-//         console.log(accounts);
-//     });
-// });
+let accounts;
+let inbox;
 
 
-class Car {
-    park() {
-        return 'stopped';
-    }
+beforeEach(async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
 
-    drive(){
-        return 'vroom';
-    }
-}
+    // User one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy( { data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000'});
+});
 
-describe('Car', ()=> {
-    it('can park', () => {
-        const car = new Car();
-        assert.equal(car.park(), 'stopped');
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
     });
-})
+});
