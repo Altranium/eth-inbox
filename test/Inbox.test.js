f637e6f2348656f24c0196c93ed8a77946466632
cyfeyr <dev@protoalpha.co>
2020-03-23 22:05:28 -0500

    Add mocha test framework

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,4 +2,42 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-                                                                                                                                
+// const { interface, bytecode } = require('../compile');
+
+// let accounts;
+// let inbox;
+
+
+// beforeEach(async () => {
+//     // Get a list of all accounts
+//     accounts = await web3.eth.getAccounts();
+
+//     // User one of those accounts to deploy the contract
+//     // inbox = await new web3.eth.Contract(JSON.parse(interface))
+//     //     .deploy( { data: bytecode, arguments: ['Hi there!'] })
+//     //     .send({ from: accounts[0], gas: '1000000'});
+// });
+
+// describe('Inbox', () => {
+//     it('deploys a contract', () => {
+//         console.log(accounts);
+//     });
+// });
+
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive(){
+        return 'vroom';
+    }
+}
+
+describe('Car', ()=> {
+    it('can park', () => {
+        const car = new Car();
+        assert.equal(car.park(), 'stopped');
+    });
+})
