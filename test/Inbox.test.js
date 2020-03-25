825454ad09cbf90c2b4946348eee5527a8ed9d2e
Alan Tran <dev@protoalpha.co>
2020-03-24 23:50:45 -0500

    Add test cases

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -13,13 +13,25 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
     // User one of those accounts to deploy the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy( { data: bytecode, arguments: ['Hi there!'] })
-        .send({ from: accounts[0], gas: '1000000'});
-});
+    inbox = await new web3.eth.Contract(JSON.parse(interface))  // Teaches web3 about what methods an Inbox contract has
+        .deploy( { data: bytecode, arguments: ['Hi there!'] })  // Tells web3 to deploy a copy of this contract
+        .send({ from: accounts[0], gas: '1000000'});            // Instructs web3 to send out a transaction that creates the contract
+
+    });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);   // Checks if it contains a non-null value
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
     });
 });
